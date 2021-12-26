ea31c320d82f42f1184feda0b57edd0e01970b08
numb <1484963022@qq.com>
2021-12-27 00:35:43 +0800

    review微服务优化

--- file/src/test/java/top/camsyn/store/file/FileApplicationTests.java
+++ file/src/test/java/top/camsyn/store/file/FileApplicationTests.java
@@ -1,13 +1,25 @@
-package camsyn.top.store.file;
+package top.camsyn.store.file;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import top.camsyn.store.file.service.FileService;
+
+import java.io.File;
+
 
 @SpringBootTest
 class FileApplicationTests {
 
+    @Autowired
+    private FileService fileService;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        File file = fileService.downloadFileByURL("https://www.baidu.com/img/540-258_b7ccca84a418140d9ac666e10b82ac27.gif");
+        System.out.println(file.getName());
+        System.out.println(file.canRead());
+        System.out.println(file.getAbsolutePath());
     }
 
 }
