285c4acb90067df31c7ee2763ddfc3337ec386cd
numb <1484963022@qq.com>
2021-12-29 23:21:00 +0800

    review 测试

--- review/src/test/java/top/camsyn/store/review/ReviewApplicationTests.java
+++ review/src/test/java/top/camsyn/store/review/ReviewApplicationTests.java
@@ -14,7 +14,8 @@ class ReviewApplicationTests {
 
     @Test
     void contextLoads() {
-        reviewLogService.updateByRid("28","0");
+        reviewLogService.selectByRid("28");
+//        reviewLogService.updateByRid("28","0");
     }
 
 }
